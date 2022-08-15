16a310dd93bd560c3bc5a51e66dae8b6ba0ff082
flightman69 <flightman69420@gmail.com>
2022-08-15 13:39:53 +0530

    added comments

--- C/diamond_pattern_w_num_diagonals.c
+++ C/diamond_pattern_w_num_diagonals.c
@@ -7,13 +7,14 @@ int main()
     printf("Enter number of rows\n");
     scanf("%d", &number);
     count = number - 1;
+	// Printing the upper triangle of the diamond
     for (k = 1; k < number; k++)
     {
         for (i = 1; i <= count+1; i++)
             printf(" ");
         count--;
         for (i = 1; i <= 2 * k - 1; i++){
-	    if(i == k){
+	    if(i == k){ // Printing numbers in the middle of upper triangle
 		    printf("%d",(k-1)%10);
 	    }else{
             printf("*");
@@ -22,18 +23,20 @@ int main()
         printf("\n");
      }
      printf(" ");	
+	 // Printing numbers in the middle of the diamond
      for(int s = 0;s<number*2-1;s++){
      	printf("%d",s%10);
      } 
      printf("\n");
      count = 1;
+	 // Printing the lower triangle of the diamond
      for (k = 1; k <= number - 1; k++)
      {
          for (i = 1; i <= count+1; i++)
              printf(" ");
          count++;
          for (i = 1 ; i <= 2 *(number - k)-  1; i++){
-	     if(i == number-k){
+	     if(i == number-k){ // Printing the numbers in the middle of lower triangle
 	         printf("%d",(number-k-1)%10);
 		}else{		
              printf("*");
