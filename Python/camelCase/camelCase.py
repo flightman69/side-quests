7071741ee7e33e4368dbd61e63b96ffbc1029431
flightman69 <flightman69420@gmail.com>
2022-10-09 20:31:24 +0530

    Added comments

--- Python/camelCase/camelCase.py
+++ Python/camelCase/camelCase.py
@@ -1,24 +1,29 @@
 import os
 
-dirList = os.listdir("/tmp/p")
+print("Converting files with _ to camelCase ....")
+
+dirList = os.listdir(os.getcwd())
+## Store all the files in the folder to the variable
 underScoreFiles = []
 for i in dirList:
-	if '_' in i:
+	if '_' in i: ## Adds the files with '_' to a list 
 		underScoreFiles.append(i)
 
-firstWord = []
 titleCase = []
 for i in underScoreFiles:
-	splitName = i.split("_")
+	splitName = i.split("_") ## Splits the file names with _
 	name = ""
 	for i in splitName:
 			name += i.title()
-	titleCase.append(name)
+	titleCase.append(name) ##Convert all seperate words to Title case and add to a list
 
 camelCase = []	
 for i in titleCase:
 	iniUpper = i[0][0]
 	iniLower = iniUpper.lower()
-	camelCase.append(i.replace(iniUpper,iniLower,1))
+	camelCase.append(i.replace(iniUpper,iniLower,1)) ##Convert the first letter from Upper to Lower case
+
 for i in range(len(camelCase)):
-	os.rename(underScoreFiles[i],camelCase[i])
+	os.rename(underScoreFiles[0],camelCase[0]) ##Rename Everything
+
+print("Succesfully converted everything :)")
